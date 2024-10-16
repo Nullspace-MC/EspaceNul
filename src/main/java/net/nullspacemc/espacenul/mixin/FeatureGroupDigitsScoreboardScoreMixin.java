02b1f8026fd1e98bf17db49bdf20b6de3bf7c1ef
SRAZKVT <sarah.mathey66666@gmail.com>
2024-10-16 18:29:33 +0200

    Finish feature: groupDigitsScoreboardScore, playerList and belowName scoreboard slots now also have digits grouped

--- src/main/java/net/nullspacemc/espacenul/mixin/FeatureGroupDigitsScoreboardScoreMixin.java
+++ src/main/java/net/nullspacemc/espacenul/mixin/FeatureGroupDigitsScoreboardScoreMixin.java
@@ -3,12 +3,12 @@ package net.nullspacemc.espacenul.mixin;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.gui.GameGui;
+import net.minecraft.client.render.entity.PlayerEntityRenderer;
+import net.nullspacemc.espacenul.util.StringFormattingUtils;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
-
-// TODO: format in playerlist slot and belowname
+import org.spongepowered.asm.mixin.injection.Slice;
 
 @Environment(EnvType.CLIENT)
 public class FeatureGroupDigitsScoreboardScoreMixin {
@@ -18,13 +18,36 @@ public class FeatureGroupDigitsScoreboardScoreMixin {
 			method = "renderSidebarObjective",
 			at = @At(value = "INVOKE", target = "Ljava/lang/StringBuilder;append(I)Ljava/lang/StringBuilder;")
 		)
-		private StringBuilder scoreboardStringGroupDigits(StringBuilder instance, int i) {
-			return instance.append(espacenul$formatGroupedDigits(i));
+		private StringBuilder scoreboardStringGroupDigitsOnSidebar(StringBuilder instance, int i) {
+			return instance.append(StringFormattingUtils.formatGroupedDigits(i));
 		}
 
-		@Unique
-		private String espacenul$formatGroupedDigits(int i) {
-			return String.format("%,d", i);
+		@Redirect(
+			method = "render",
+			at = @At(
+				value = "INVOKE",
+				target = "Ljava/lang/StringBuilder;append(I)Ljava/lang/StringBuilder;"
+			),
+			slice = @Slice(
+				from = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/scoreboard/Scoreboard;getScore(Ljava/lang/String;Lnet/minecraft/scoreboard/ScoreboardObjective;)Lnet/minecraft/scoreboard/ScoreboardScore;"
+				)
+			)
+		)
+		private StringBuilder scoreboardStringGroupDigitsOnPlayerList(StringBuilder instance, int i) {
+			return instance.append(StringFormattingUtils.formatGroupedDigits(i));
+		}
+	}
+
+	@Mixin(PlayerEntityRenderer.class)
+	abstract static class PlayerEntityRendererMixin {
+		@Redirect(
+			method = "renderNameTag(Lnet/minecraft/client/entity/living/player/ClientPlayerEntity;DDDLjava/lang/String;FD)V",
+			at = @At(value = "INVOKE", target = "Ljava/lang/StringBuilder;append(I)Ljava/lang/StringBuilder;")
+		)
+		private StringBuilder scoreboardStringGroupDigitsBelowName(StringBuilder instance, int i) {
+			return instance.append(StringFormattingUtils.formatGroupedDigits(i));
 		}
 	}
 }
---
+++ src/main/java/net/nullspacemc/espacenul/util/StringFormattingUtils.java
@@ -0,0 +1,7 @@
+package net.nullspacemc.espacenul.util;
+
+public class StringFormattingUtils {
+	public static String formatGroupedDigits(int i) {
+		return String.format("%,d", i);
+	}
+}
